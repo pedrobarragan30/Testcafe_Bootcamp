2c74dbf4632bc7e65c2218de8d0dc4562312272c
PedroB <pedrobarragan30@gmail.com>
2020-10-15 21:32:03 -0500

    Primeras practicas

---
+++ Javascript/bucles.js
@@ -0,0 +1,14 @@
+//Bucles
+
+let i = 0
+
+while(i < 5)
+{
+    console.log("Este es el valor de i en el while", i, "y asi con el tiempo")
+    i++
+}
+
+for (i=0; i<5 ;i++)
+{
+    console.log("Este es el valor de i en el for", i)
+}
